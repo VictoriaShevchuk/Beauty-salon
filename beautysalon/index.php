5168cfa5a8381a8175ead4fcfa07d01bf5bab296
Нелли <160469487+13563457@users.noreply.github.com>
2024-03-03 17:46:38 +0300

    Update index.php

--- beautysalon/index.php
+++ beautysalon/index.php
@@ -3,6 +3,11 @@ include 'template/head.php';
 include 'template/nav.php'; 
 include 'template/data.php';
 ?>
+   <div class="row">
+          <div class="col-lg-12">
+            <div class="fon1">fon1</div>  
+          </div>
+        </div>
 <?php 
 include 'template/footer.php'; 
 ?>
