e0fafdc4efbeff82897be850792f11908cad3bf6
Нелли <160469487+13563457@users.noreply.github.com>
2024-03-03 13:18:56 +0300

    Update head.php

--- beautysalon/template/head.php
+++ beautysalon/template/head.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,3 +10,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title><i>Салон красоты Bellezza</i></title>
 </head>
+<body>
