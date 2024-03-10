df53af9cd64ddbfc91991dd4076672cdc201084d
Нелли <160469487+13563457@users.noreply.github.com>
2024-03-10 16:20:00 +0300

    Update aboutus.php

--- beautysalon/aboutus.php
+++ beautysalon/aboutus.php
@@ -3,7 +3,111 @@ include 'template/head.php';
 include 'template/nav.php'; 
 include 'template/data.php';
 ?>
-
+<main>
+  <h1 class="name">Sanqqbar</h1>
+    <section id="salon">
+        <div class="row">
+         <div class="col-lg-1"></div> 
+          <div class="card col-lg-10" >
+           <div class="row">
+            <div class="col-lg-5">
+             <img src="images/салон красоты 1.png" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-lg-7">
+              <div class="card-body">
+               <p class="card-text">В салоне красоты «Sanqqbar» выполняют полный спектр услуг по уходу за лицом, телом
+                <br> и волосами, которые помогут изменить свой образ, придать внешнему виду ухоженность, красоту и очарование.
+                 <br>В Sanqqbar вас встретят современные высококвалифицированные мастера-стилисты,которые регулярно повышают
+                 <br>  свой профессиональный уровень в самых лучших школах, на престижных семинарах и мастер-классах. 
+               <br> Мы используем только профессиональную косметику класса люкс: Kydra, Alterna, Olaplex, Secret Pro.
+               <br> Помимо парикмахерских услуг Вам предложат широкий выбор самых передовых методик в области косметологии,
+               <br>  массажа,маникюра педикюра и моделирования ногтей.
+              </div>
+             </div>
+            </div>
+           </div>
+          <div class="col-lg-1"></div> 
+        </div>
+      </section>
+      <h1 class="name">Наши преимущества</h1>
+      <section id="salon">
+       <div class="row">
+        <div class="col-lg-12">
+         <div class="row">
+          <div class="col-lg-2"></div>  
+          <div class="col-lg-2">
+           <img src="images/20.png" class="card-img-top" alt="...">
+          </div>
+          <div class="col-lg-2">
+           <img src="images/17.png" class="card-img-top" alt="...">  
+          </div>
+          <div class="col-lg-2">
+           <img src="images/18.png" class="card-img-top" alt="...">
+          </div>
+          <div class="col-lg-2">
+           <img src="images/19.png" class="card-img-top" alt="...">
+          </div>
+          <div class="col-lg-2"></div> 
+        </div>
+        <div class="row">
+         <div class="col-lg-2"></div>  
+          <div class="col-lg-2">
+           <p class="text">Доброжелательный<br>коллектив</p>  
+          </div>
+          <div class="col-lg-2">
+           <p class="text">Уютная атмосфера</p>  
+          </div>
+          <div class="col-lg-2">
+           <p class="text">Высококвалифицированные мастера</p>  
+          </div>
+          <div class="col-lg-2">
+           <p class="text">Невысокие цены на качественные услуги</p>  
+          </div>
+          <div class="col-lg-2"></div> 
+         </div>
+        </div>
+       </div>
+      </section>
+      <h1 class="name">Наш салон</h1>
+      <section id="salon">
+       <div class="row">
+       <div class="col-lg-12">
+         <div class="row">
+          <div class="col-lg-2"></div>
+          <div class="col-lg-2">
+          <img src="images/10.png" class="img-same-size" alt="...">
+          </div>
+          <div class="col-lg-2">
+          <img src="images/11.png" class="img-same-size" alt="...">
+          </div>
+          <div class="col-lg-2">
+          <img src="images/12.png" class="img-same-size" alt="...">
+          </div>
+          <div class="col-lg-2">
+          <img src="images/13.png" class="img-same-size" alt="...">
+          </div>
+          <div class="col-lg-2"></div> 
+         </div>
+         <div class="row">
+          <div class="col-lg-2"></div>
+          <div class="col-lg-2">
+          <img src="images/14.png" class="img-same-size" alt="...">
+          </div>
+          <div class="col-lg-2">
+          <img src="images/15.png" class="img-same-size" alt="...">
+          </div>
+          <div class="col-lg-2">
+          <img src="images/16.png" class="img-same-size" alt="...">
+          </div>
+          <div class="col-lg-2">
+          <img src="images/21.png" class="img-same-size" alt="...">
+          </div>
+          <div class="col-lg-2"></div> 
+         </div>
+         </div>
+        </div>
+      </section>
+</main> 
 <?php 
 include 'template/footer.php'; 
 ?>
