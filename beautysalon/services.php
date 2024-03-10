4f623064e2d6b8485cd54efb513492e3ca492e24
Нелли <160469487+13563457@users.noreply.github.com>
2024-03-10 16:24:14 +0300

    Update services.php

--- beautysalon/services.php
+++ beautysalon/services.php
@@ -3,6 +3,57 @@ include 'template/head.php';
 include 'template/nav.php'; 
 include 'template/data.php';
 ?>
+<div class="container">
+<div class="row">
+<div class="col-12">  
+<h1 class="name">Услуги салона</h1>
+</div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-2"></div>
+
+<div class="col-8">  
+<div class="card-group">
+  <div class="card">
+    <img src="images/9.png" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">Волосы</h5>
+      <p class="card-text">Окрашивание волос — процесс, в ходе которого наносится специальное красящее вещество, способное изменить природный цвет или оттенок.<br>
+Стрижка огнём — способ ухода за волосами, заключающийся в воздействии открытого огня на стержень волоса.<br>
+Стрижка волос — укорачивание волос. Процесс стрижки состоит из методов, операций, срезов, определенного фасона.<br>
+Мелирование — окрашивание волос отдельными прядями.</p>
+<a href="hair.php">
+<button type="button" class="btn bg-danger bg-gradient">Узнать больше</button>
+</a>
+    </div>
+  </div>
+  <div class="card">
+    <img src="images/2.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Ногти</h5>
+      <p class="card-text">Маникюр — уход за пальцами рук, чистка, полировка, покрытие лаком ногтей.<br>
+Педикюр — уход за ступнями и пальцами ног (чистка и полировка ногтей, удаление мозолей и т. п.).</p>
+<a href="nails.php">
+<button type="button" class="btn bg-danger bg-gradient">Узнать больше</button>
+</a>
+    </div>
+  </div>
+  <div class="card">
+    <img src="images/mas1.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Массаж</h5>
+      <p class="card-text">Массаж — совокупность приёмов механического и рефлекторного воздействия на ткани и органы в виде растирания, давления, вибрации, проводимых непосредственно на поверхности тела человека как руками, так и специальными аппаратами через воздушную, водную или иную среду с целью достижения лечебного или иного эффекта.</p>
+<a href="massage.php">
+<button type="button" class="btn bg-danger bg-gradient">Узнать больше</button>
+</a>
+    </div>
+  </div>
+</div>
+</div>
+<div class="col-2"></div>
+</div>
 
 <?php 
 include 'template/footer.php'; 
