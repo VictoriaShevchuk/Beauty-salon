0cb6af263ffa53cc25b2b5a1279e9278ca1c075f
Нелли <160469487+13563457@users.noreply.github.com>
2024-03-10 16:22:22 +0300

    Update contacts.php

--- beautysalon/contacts.php
+++ beautysalon/contacts.php
@@ -3,7 +3,23 @@ include 'template/head.php';
 include 'template/nav.php'; 
 include 'template/data.php';
 ?>
+<main> 
+<h1 class="name">Контакты</h1>
+      <section id="salon">
+       <div class="row">
+        <div class="col-lg-12">
+         <div class="row">
+          <div class="col-lg-2"></div>
+          <div class="col-lg-8">
 
+
+          </div>
+          <div class="col-lg-2"></div> 
+         </div>
+        </div>
+       </div>
+      </section>
+</main>
 <?php 
 include 'template/footer.php'; 
 ?>
