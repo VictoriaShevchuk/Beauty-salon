ddde17ee16bb79ee42835b54f21d8908086e8440
Нелли <160469487+13563457@users.noreply.github.com>
2024-03-11 13:38:59 +0300

    Update nav.php

--- beautysalon/template/nav.php
+++ beautysalon/template/nav.php
@@ -1,39 +1,48 @@
 <header>
-<div class="row">
-<div class="col-lg-4"></div>
-<div class="col-lg-4">  
-<h3 class="salon">Салон красоты <br>Bellezza</h3>
-</div>
-<div class="col-4"></div>
-  <div class="col-lg-12">  
-    <nav class="navbar navbar-expand-lg">
+  <div class="row">
+      <div class="col-lg-2">
+        <img src="images/logo3.png"width="170"height="90">  
+       </div>
+          <div class="col-lg-7">
+            <nav class="navbar navbar-expand-lg">
               <div class="container-fluid justify-content-center ">
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Переключатель навигации">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse flex-grow-0" id="navbarNav">
-                    <div class="menu">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Переключатель навигации">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse flex-grow-0" id="navbarNav">
+                  <div class="menu">
                     <ul class="navbar-nav ">
                       <li class="nav-item">
-                        <a class="nav-link active" href="#index">Главная</a>
+                        <a class="nav-link active" href="index.php"style="color:rgb(243, 13, 120);"><b>ГЛАВНАЯ</b></a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link active" href="#services">Услуги</a>
+                        <a class="nav-link active" href="services.php"style="color:rgb(243, 13, 120);"><b>УСЛУГИ</b></a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link active" href="#staff">Сотрудники</a>
+                        <a class="nav-link active" href="staff.php"style="color:rgb(243, 13, 120);"><b>СОТРУДНИКИ</b></a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link active" href="#aboutus">О нас</a>
+                        <a class="nav-link active" href="aboutus.php"style="color:rgb(243, 13, 120);"><b>О НАС</b></a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link active" href="#contacts">Контакты</a>
-                      </li>
+                        <a class="nav-link active" href="contacts.php"style="color:rgb(243, 13, 120);"><b>КОНТАКТЫ</b></a>
+                      </li> 
                     </ul>
                   </div>
                 </div>
-              </div>
-           </nav>
-       </div>
-   </div>
+               </div>
+            </nav>
+          </div>
+     <div class="col-lg-1"> 
+      <div class="lichka">
+       <a href="lichka.php"><img src="images/22.png" alt="Личный"style="width:40px"></a>
+      </div>
+     </div>
+     <div class="col-lg-2"> 
+      <div class="phone">
+       <p>Звоните нам прямо <br>сейчас: </p>
+       <p class="numb"><b> 8 (342)274-55-55</b></p>
+      </div>
+     </div>
+  </div>
 </header> 
